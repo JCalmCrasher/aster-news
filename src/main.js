ca43e4221158eea326402e19a91e737951f01205
joshx <joshua.osagie01@gmail.com>
2022-10-15 13:15:19 +0100

    remove `fontawesome`

--- src/main.js
+++ src/main.js
@@ -2,14 +2,6 @@ import { createApp } from "vue";
 import "./style.css";
 import App from "./App.vue";
 
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { faHouse } from "@fortawesome/free-solid-svg-icons";
-import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
-
-library.add(faHouse);
-
 const app = createApp(App);
-
-app.component("font-awesome-icon", FontAwesomeIcon);
 
 app.mount("#app");
