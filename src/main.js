f826185b0962d035e113558ddca43b241f63b661
joshx <joshua.osagie01@gmail.com>
2022-10-15 02:22:02 +0100

    deps: `fontawesome`

--- src/main.js
+++ src/main.js
@@ -1,5 +1,15 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from "vue";
+import "./style.css";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+
+library.add(faHouse);
+
+const app = createApp(App);
+
+app.component("font-awesome-icon", FontAwesomeIcon);
+
+app.mount("#app");
