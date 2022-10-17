015dc62ccd9a027d4b5e8b7e98913f433360d8e3
unknown <asarebrave@gmail.com>
2022-10-17 14:01:04 +0100

    Feat:
    Design of the Top story section

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,8 @@
 <template>
   <the-sidebar />
+  <header class="sm:ml-[260px]">
+    <TheTopStory/>
+  </header>
   <main class="sm:ml-[260px]">
     <div class="w-full sm:w-[771px]">
       <ArticleFeatureCard />
@@ -15,8 +18,9 @@
 import TheSidebar from "./components/the-sidebar.vue";
 import ArticleFeatureCard from "./components/article-feature-card.vue";
 import ArticleSingleCard from "./components/article-single-card.vue";
+import TheTopStory from "./components/the-top-story.vue";
 export default {
-  components: { TheSidebar, ArticleFeatureCard, ArticleSingleCard },
+  components: { TheSidebar, ArticleFeatureCard, ArticleSingleCard, TheTopStory },
 };
 </script>
 
---
+++ src/components/the-top-story.vue
@@ -0,0 +1,28 @@
+<template lang="">
+    <div>
+        <div class="mb-[22px]">
+            <h1 class="text-xl font-bold text-black">Top Stories for you</h1>
+        </div>
+
+        <div class="space-x-[10px] mb-6  flex flex-row">
+            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">All</button>
+            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">Andriod</button>
+            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">Cricket</button>
+            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">Iphone</button>
+            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">Google</button>
+            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">Nano Technology</button>
+            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">Mental Health</button>
+            <div class="flex flex-row items-center">
+            <i class="fa fa-ellipsis-h fa-lg self-center ml-[22px]" aria-hidden="true"></i>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:"TopStory"
+}
+</script>
+<style lang="">
+    
+</style>
