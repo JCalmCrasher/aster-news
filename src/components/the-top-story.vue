8dd66fad1178167ff677e14ecfaf43014f403f97
unknown <asarebrave@gmail.com>
2022-10-18 00:57:00 +0100

    Fix: made some slight adjustment and restrutured the code base

--- src/components/the-top-story.vue
+++ src/components/the-top-story.vue
@@ -5,13 +5,13 @@
         </div>
 
         <div class="space-x-[10px] mb-6  flex flex-row">
-            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">All</button>
-            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">Andriod</button>
-            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">Cricket</button>
-            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">Iphone</button>
-            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">Google</button>
-            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">Nano Technology</button>
-            <button class=" bg-white text-sm rounded-3xl px-4 py-2 hover:text-white hover:bg-sky-blue">Mental Health</button>
+            <button class="btn-regular bg-sky-blue text-white">All</button>
+            <button class="btn-regular">Andriod</button>
+            <button class="btn-regular">Cricket</button>
+            <button class="btn-regular">Iphone</button>
+            <button class="btn-regular">Google</button>
+            <button class="btn-regular">Nano Technology</button>
+            <button class="btn-regular">Mental Health</button>
             <div class="flex flex-row items-center">
             <i class="fa fa-ellipsis-h fa-lg self-center ml-[22px]" aria-hidden="true"></i>
             </div>
