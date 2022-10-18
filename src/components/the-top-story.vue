818a6c6b71032d11f9b976dde0ca87e0428cd757
unknown <asarebrave@gmail.com>
2022-10-18 19:49:13 +0100

    Fix: Added some mobile responsiveness

--- src/components/the-top-story.vue
+++ src/components/the-top-story.vue
@@ -4,16 +4,16 @@
             <a class="text-xl font-bold text-black">Top Stories for you</a>
         </div>
 
-        <div class="space-x-[10px] mb-6  flex flex-row">
+        <div class="sm:space-x-[10px] mb-6 grid grid-cols-3 gap-3 sm:flex sm:flex-row">
             <button class="btn-regular bg-sky-blue text-white">All</button>
             <button class="btn-regular">Andriod</button>
             <button class="btn-regular">Cricket</button>
             <button class="btn-regular">Iphone</button>
             <button class="btn-regular">Google</button>
-            <button class="btn-regular">Nano Technology</button>
-            <button class="btn-regular">Mental Health</button>
+            <button class="btn-regular">Games </button>
+            <button class="btn-regular">Health</button>
             <div class="flex flex-row items-center">
-            <i class="fa fa-ellipsis-h fa-lg self-center ml-[22px]" aria-hidden="true"></i>
+            <i class="fa fa-ellipsis-h fa-lg self-center ml-[22px] block" aria-hidden="true"></i>
             </div>
         </div>
     </div>
