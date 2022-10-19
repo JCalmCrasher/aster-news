cec1cc410fc8e3aa64007b527436a751e5880d8b
joshx <joshua.osagie01@gmail.com>
2022-10-19 08:40:56 +0100

    refactor: `img`

--- src/components/article-single-card.vue
+++ src/components/article-single-card.vue
@@ -14,7 +14,7 @@
         </p>
       </div>
       <div class="mx-[17px]">
-        <img src="../img/battleground.png" alt="" class="" />
+        <img src="../img/battleground.png" alt="battleground" />
       </div>
     </div>
     <div class="flex flex-row mt-2 box-border mb-[13px]">
