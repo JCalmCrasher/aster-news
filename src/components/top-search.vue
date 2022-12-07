4d9a8704d6c749a1b9de7bcc78405689c58010c2
unknown <asarebrave@gmail.com>
2022-12-07 14:06:47 +0100

    fix: made adjustment to the search box border

--- src/components/top-search.vue
+++ src/components/top-search.vue
@@ -2,7 +2,7 @@
     <div class="pt-2 mb-4 lg:pt-[15px] lg:mb-[38px]">
        <div class="flex flex-row items-center justify-between px-2 sm:mr-[34px]">
         <div class="flex flex-row">
-            <input type="text" name="" id="" placeholder="Search for news..." class="rounded  pl-[10px] py-[8px] bg-light-blue outline-none text-xs box-border text-black sm:w-[170px] lg:w-[455px] lg:px-[14px] lg:py-[14px] lg:text-sm focus:border">
+            <input type="text" name="" id="" placeholder="Search for news..." class="rounded border border-blue pl-[10px] py-[8px] bg-light-blue outline-none text-xs box-border text-black sm:w-[230px] lg:w-[455px] lg:px-[14px] lg:py-[14px] lg:text-sm  focus:border focus:border-black">
             <div class="relative flex flex-row items-center">
                 <div class="absolute right-0">
                     <a href="#"> <i class="fa fa-search mx-4 hover:text-sky-blue" aria-hidden="true"></i></a>
@@ -16,7 +16,7 @@
         </div>
         <div class="text-black  text-sm  flex flex-row items-center">
             <i class="fa fa-user-o fa-lg" aria-hidden="true"></i>
-            <h1 class="pl-[17px] sm:pl-[10px] hidden sm:block text-sm">My Profile</h1>  
+            <h1 class="pl-[17px] sm:pl-[10px] hidden lg:block text-sm">My Profile</h1>  
             
         </div>
        </div>
