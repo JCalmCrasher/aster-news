39d8524e6f38ef6f04c9ab77ac4e4b34828503c0
unknown <asarebrave@gmail.com>
2023-02-10 23:45:11 +0100

    fix: fix the hamburger component

--- src/components/hambuger.vue
+++ src/components/hambuger.vue
@@ -1,6 +1,6 @@
 <template>
     <div>
-        <div class="absolute w-screen h-screen bg-white z-[99] p-3 space-y-3 flex flex-col bottom-0 text-lg sm:hidden">
+        <div class="fixed w-screen h-full top-10 bg-white z-[99] p-3 space-y-3 flex flex-col bottom-0 text-lg sm:hidden">
             <div class="text-black text-sm  flex flex-row items-center justify-end">
                 <i class="fa fa-user-o fa-lg" aria-hidden="true"></i>
                 <h1 class="pl-[17px] block text-sm">My Profile</h1>
