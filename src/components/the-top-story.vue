352db288ef2695bf64d219fba54a59c8c7b0c612
unknown <asarebrave@gmail.com>
2023-02-18 22:04:55 +0100

    fix: adjust the bg property for the index.html file

--- src/components/the-top-story.vue
+++ src/components/the-top-story.vue
@@ -6,12 +6,6 @@
 
         <div class="mx-4 mb-6 grid grid-cols-3 gap-3 sm:mx-0 lg:flex lg:flex-row lg:space-x-[10px]">
             <button class="btn-regular bg-sky-blue text-white">All</button>
-            <!-- <button class="btn-regular" ref="andy" @click="getIfAndriodClick">Andriod</button>
-            <button class="btn-regular">Cricket</button>
-            <button class="btn-regular">Iphone</button>
-            <button class="btn-regular">Google</button>
-            <button class="btn-regular">Games </button>
-            <button class="btn-regular">Health</button> -->
             <button class="btn-regular" v-for="button in buttons" @click="getButtonText(button)">{{ button }}</button>
 
             <div class="flex flex-row items-center">
@@ -33,6 +27,7 @@ export default {
     methods: {
         getButtonText(button) {
             this.$emit('topStoryButton', button)
+            // button.innerHtml.
         }
     }
 }
